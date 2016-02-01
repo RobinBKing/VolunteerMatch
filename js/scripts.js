ca01616e383e34b40bb34486b4b4c26603a0d3b9
Jacob Steinberg and Rob King <samiamspop@gmail.com>
2016-02-01 14:13:48 -0800

    first two successful tests on Organization construction

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 //business logic
-
+function Organization(organizationName, organizationDescription, street, city, state, zip, phone, website){
+  this.organizationName =  organizationName;
+  this.organizationDescription = organizationDescription;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  this.phone = phone;
+  this.website = website;
+};
+Organization.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+  };
 //interface
-$(document).ready(function() {
-});
+// $(document).ready(function() {
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,17 @@
-describe('template', function() {
-  it("is true/fals some behavior", function() {
-    expect(notTriangle(10, 10, 20)).to.equal(true);
-  });
+describe('Organization', function() {
+  it("creates a new orgainzation with given properties", function() {
+     var testOrgization = new Organization("Portland Rescue Mission", "A place for homless people", "123 main st.", "Portland", "Oregon", "97123", "503-313-1234", "http://www.PortlandRescueMission.org");
+     expect(testOrgization.organizationName).to.equal("Portland Rescue Mission");
+     expect(testOrgization.organizationDescription).to.equal("A place for homless people");
+     expect(testOrgization.street).to.equal("123 main st.");
+     expect(testOrgization.city).to.equal("Portland");
+     expect(testOrgization.state).to.equal("Oregon");
+     expect(testOrgization.zip).to.equal("97123");
+     expect(testOrgization.phone).to.equal("503-313-1234");
+     expect(testOrgization.website).to.equal("http://www.PortlandRescueMission.org");
+   });
+   it("adds the address information into one string", function() {
+     var testOrgization = new Organization("Portland Rescue Mission", "A place for homless people", "123 main st.", "Portland", "Oregon", "97123", "503-313-1234", "http://www.PortlandRescueMission.org");
+     expect(testOrgization.fullAddress()).to.equal("123 main st., Portland, Oregon 97123");
+   });
 });
