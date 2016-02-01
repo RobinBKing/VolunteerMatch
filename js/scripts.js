80883e4698544049181483af1e86ee2d243b6411
Pablo Vera and Ryan Brown <browneryan87@gmail.com>
2016-02-01 15:33:43 -0800

    add carousel, header, phots, and a sidebar to the index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-function initMap() {
-  var mapDiv = document.getElementById('google-map');
-  var map = new google.maps.Map(mapDiv, {
-  center: {lat: 44.540, lng: -78.546},
-    zoom: 8
-  });
-}
 
-//business logic
-
-//interface
 $(document).ready(function() {
 });
--- spec/specs.js
+++
@@ -1,5 +0,0 @@
-describe('template', function() {
-  it("is true/fals some behavior", function() {
-    expect(notTriangle(10, 10, 20)).to.equal(true);
-  });
-});
