733e9a09bc353759914b8d0178a0822555d4a064
Jacob Steinberg and Rob King <samiamspop@gmail.com>
2016-02-02 12:02:54 -0800

    add API and tested

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,17 @@ function OrganizationGroup(){
   this.organizations = [];
 }
 //interface
-// $(document).ready(function() {
-// });
+$(document).ready(function() {
+  var location = "Portland, OR"
+  var distance = 15;
+  var keyword = "homelessness"
+  $.getJSON("http://api2.allforgood.org/api/volopps?key=epicodus&type=all&merge=3&output=json-hoc&vol_loc=" + location + "&vol_dist=" + distance + "&vol_startdate=NOW&q=(" + keyword + ")", function(json) {
+    if (json != "Nothing found."){
+      console.log(json);
+      alert(json.items[0].location_name);
+    }
+    else {
+      alert("No results found. Try changing your search criteria.")
+    };
+  });
+});
