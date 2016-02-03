3f8ac3e894a66a4a74d7b2b87f7e436d0c4a2aba
Jake Steinberg and Rob King <samiamspop@gmail.com>
2016-02-03 15:16:15 -0800

    add display of data

--- js/scripts.js
+++ js/scripts.js
@@ -57,8 +57,8 @@ $(document).ready(function() {
     event.preventDefault();
     var startRecord = 0;
     var sort = "geodist() asc";
-    var recordsReturn = 1;
-    var location = "Portland, OR";
+    var recordsReturn = 100;
+    var location = "97006";
     var distance = 15;
     var startDate = "NOW";
     var endDate = "&vol_enddate=" + "2016-07-29";
@@ -66,5 +66,21 @@ $(document).ready(function() {
 
     var newOpportunities = new Opportunities(recordsReturn, location, distance, startDate, endDate, keyword);
     jsonOportunities(newOpportunities);
+    for (var i = 0; i < newOpportunities.items.length; i++) {
+      newOpportunities.items[i];
+      var newDistance = parseFloat(newOpportunities.items[i].Distance).toFixed(2);
+      $("#newOpportunities").append("<h4>" + newOpportunities.items[i].title + "</h4>" +
+                                    "<p>" +  newOpportunities.items[i].description + "</p>" +
+                                    "<ul>" +
+                                      "<li>"+newOpportunities.items[i].location_name +"</li>" +
+                                      "<li>"+ newDistance +"</li>" +
+                                      "<li><a href='" + newOpportunities.items[i].detailUrl + "'>" + newOpportunities.items[i].detailUrl + "</a></li>" +
+                                      "<li>"+newOpportunities.items[i].startDate +"</li>" +
+                                      "<li>"+newOpportunities.items[i].endDate +"</li>" +
+                                      "<li>"+newOpportunities.items[i].volunteerHubOrganizationName +"</li>" +
+                                      "<li><a href='" + newOpportunities.items[i].volunteerHubOrganizationUrl + "'>" + newOpportunities.items[i].volunteerHubOrganizationUrl + "</a></li>" +
+                                    "<ul>");
+    };
+
    });
 });
