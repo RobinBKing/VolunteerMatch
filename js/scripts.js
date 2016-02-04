07e70f8cf836ae0d924bf9dca6fc5cdad3785a96
Pablo Vera and Ryan Brown <pablovera.pdx@gmail.com>
2016-02-04 10:22:05 -0800

    Add date logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //business logic
-function Opportunities(recordsReturn, location, distance, startDate, endDate, keyword){
-  this.recordsReturn = recordsReturn;
+function Opportunities(recordsReturnMax, location, distance, startDate, endDate, keyword){
+  this.recordsReturnMax = recordsReturnMax;
+  this.recordsReturn = 0;
   this.location = location;
   this.distance = distance;
   this.startDate = startDate;
@@ -21,76 +22,106 @@ var jsonOportunities = function(newOpportunities) {
   });
 
   $.getJSON(newOpportunities.jsonString, function(jsonOpportunities) {
+    newOpportunities.totalOpportunities = jsonOpportunities.TotalOpportunities;
+    newOpportunities.recordsReturn = jsonOpportunities.num;
     if (jsonOpportunities.num > 0) {
-    //  var newOpportunities = new Opportunities(jsonOpportunities.TotalOpportunities);
-    for (var i = 0; i < jsonOpportunities.items.length; i++) {
-       newOpportunities.items.push(jsonOpportunities.items[i]);
-     };
-     newOpportunities.totalOpportunities = jsonOpportunities.TotalOpportunities;
-    //  console.log(jsonOpportunities);
-    }
-    else
-    {
-      // alert("No results found. Try changing your search criteria.")
+      for (var i = 0; i < jsonOpportunities.items.length; i++) {
+         newOpportunities.items.push(jsonOpportunities.items[i]);
+       };
     };
   });
+  $.ajaxSetup({
+  async: true
+  });
 };
-function Organization(organizationName, organizationDescription, street, city, state, zip, phone, website){
-  this.organizationName =  organizationName;
-  this.organizationDescription = organizationDescription;
-  this.street = street;
-  this.city = city;
-  this.state = state;
-  this.zip = zip;
-  this.phone = phone;
-  this.website = website;
-};
-Organization.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+var newDistanceNumber = function(newDistance){
+  var newDistanceNumber =  0;
+  switch(newDistance) {
+    case "10 Miles":
+      newDistanceNumber = 10;
+      break;
+    case "15 Miles":
+      newDistanceNumber = 15;
+    break;
+    case "20 Miles":
+      newDistanceNumber = 20;
+      break;
+    case "25 Miles":
+      newDistanceNumber = 25;
+      break;
+    case "Any":
+      newDistanceNumber = 10000;
+      break;
+    default:
+      newDistanceNumber = 10;
+      break;
   };
-function OrganizationGroup(){
-  this.organizations = [];
-}
-
-// dropdown text function
-// $("li").click(function() {
-//   alert("this is a list.");
-// });
-
+  return newDistanceNumber;
+};
 //interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    //defaults
     var startRecord = 0;
     var sort = "geodist() asc";
     var recordsReturn = 100;
-    // var location = "97006";
-    var location = $("input#location").val();
-    var distance = 15;
-    var startDate = "NOW";
-    var endDate = "&vol_enddate=" + "2016-07-29";
+    //end defaults
+    var newLocation = $("input#location").val();
+    var newDistance = newDistanceNumber($("select#distance").val());
+    var newStartdate = $("input#start-date").val();
+    var newEnddate = $("input#end--date").val();
+    //If opportunity is Start Date = End Date , then display: [Start date] 	ex: June 1
+// If opportunity is End Date - Start Date <= 30  && Start Date <> End Date , then display: [Start date] - [End Date]	ex: June 1 - June 30
+// If opportunity is End Date - Start Date > 30 && End Date - Current Date > 30, then display: Ongoing
+
+// If opportunity is End Date - Start Date > 30  && Start Date <> End Date && End Date - Current Date <= 30 then display: Now until [End Date]  ex: Now until June 15
+    var startDate = "";
+    vare endDate = "";
+    // var endDate = "&vol_enddate=" + "2016-07-29";
+    if(newStartdate === newEnddate) {
+      startDate = newStartdate;
+    };
+    if((newEnddate - newStartdate <= 30) && (newStartdate != newEnddate)) {
+      startDate = newStartdate;
+      endDate = "&vol_enddate=" + newEnddate;
+    };
+    if((newEnddate - newStartdate > 30) &&  (newEnddate - currentDate > 30) {
+      startDate =  "NOW";
+      endDate = "";
+    };
+    if((newEnddate - newStartdate > 30) &&  (newStartdate != newEnddate) && (newEnddate - currentDate <= 30) {
+      startDate =  "NOW";
+      endDate = "&vol_enddate=" + newEnddate;
+    };
+
     // var keyword = "hunger homelessness OR Health & Wellness";
     var keyword = $("input#insert-text").val();
     console.log(location);
 
     var newOpportunities = new Opportunities(recordsReturn, location, distance, startDate, endDate, keyword);
     jsonOportunities(newOpportunities);
-    for (var i = 0; i < newOpportunities.items.length; i++) {
-      newOpportunities.items[i];
-      var newDistance = parseFloat(newOpportunities.items[i].Distance).toFixed(2);
-      $("#newOpportunities").append("<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 border'>" +
-                                    "<h4>" + newOpportunities.items[i].title + "</h4>" +
-                                    "<p>" +  newOpportunities.items[i].description + "</p>" +
-                                    "<ul>" +
-                                      "<li>"+newOpportunities.items[i].location_name +"</li>" +
-                                      "<li>"+ newDistance +"</li>" +
-                                      "<li><a href='" + newOpportunities.items[i].detailUrl + "'>" + "Click here for the link" + "</a></li>" +
-                                      "<li>"+newOpportunities.items[i].startDate +"</li>" +
-                                      "<li>"+newOpportunities.items[i].endDate +"</li>" +
-                                      "<li>"+newOpportunities.items[i].volunteerHubOrganizationName +"</li>" +
-                                      "<li><a href='" + newOpportunities.items[i].volunteerHubOrganizationUrl + "'>" + "Click here for info about the organization" + "</a></li>" +
-                                    "<ul></div>");
+    if(newOpportunities.recordsReturn > o){
+      for (var i = 0; i < newOpportunities.items.length; i++) {
+        newOpportunities.items[i];
+        var newDistance = parseFloat(newOpportunities.items[i].Distance).toFixed(2);
+        $("#newOpportunities").append("<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 border'>" +
+                                      "<h4>" + newOpportunities.items[i].title + "</h4>" +
+                                      "<p>" +  newOpportunities.items[i].description + "</p>" +
+                                      "<ul>" +
+                                        "<li>"+newOpportunities.items[i].location_name +"</li>" +
+                                        "<li>"+ newDistance +"</li>" +
+                                        "<li><a href='" + newOpportunities.items[i].detailUrl + "'>" + "Click here for the link" + "</a></li>" +
+                                        "<li>"+newOpportunities.items[i].startDate +"</li>" +
+                                        "<li>"+newOpportunities.items[i].endDate +"</li>" +
+                                        "<li>"+newOpportunities.items[i].volunteerHubOrganizationName +"</li>" +
+                                        "<li><a href='" + newOpportunities.items[i].volunteerHubOrganizationUrl + "'>" + "Click here for info about the organization" + "</a></li>" +
+                                      "<ul></div>");
+      };
+
+    } else {
+      alert("No results found. Try changing your search criteria.")
     };
 
-   });
+  });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -6,26 +6,9 @@ describe('Opportunities', function () {
     expect(testOpportunities.items.length).to.equal(1);
   });
 });
-describe('Organization', function() {
-  it("creates a new orgainzation with given properties", function() {
-     var testOrgization = new Organization("Portland Rescue Mission", "A place for homless people", "123 main st.", "Portland", "Oregon", "97123", "503-313-1234", "http://www.PortlandRescueMission.org");
-     expect(testOrgization.organizationName).to.equal("Portland Rescue Mission");
-     expect(testOrgization.organizationDescription).to.equal("A place for homless people");
-     expect(testOrgization.street).to.equal("123 main st.");
-     expect(testOrgization.city).to.equal("Portland");
-     expect(testOrgization.state).to.equal("Oregon");
-     expect(testOrgization.zip).to.equal("97123");
-     expect(testOrgization.phone).to.equal("503-313-1234");
-     expect(testOrgization.website).to.equal("http://www.PortlandRescueMission.org");
-   });
-   it("adds the address information into one string", function() {
-     var testOrgization = new Organization("Portland Rescue Mission", "A place for homless people", "123 main st.", "Portland", "Oregon", "97123", "503-313-1234", "http://www.PortlandRescueMission.org");
-     expect(testOrgization.fullAddress()).to.equal("123 main st., Portland, Oregon 97123");
-   });
-});
-describe('OrganizationGroup', function() {
-  it("creates an array of Organizations by given properties", function() {
-    var testOrganizationGroup = new OrganizationGroup();
-    expect(testOrganizationGroup.organizations).to.eql([]);
+describe('Distance', function () {
+  it("returns a new distance number from distanse selection", function() {
+    var testDistance = newDistanceNumber("15 Miles");
+    expect(testDistance).to.equal(15);
   });
 });
