51e62221feb794b0214fbf29f57b7cf383ce822c
RobinBKing <samimspop@gmail.com>
2016-02-04 23:18:48 -0800

    add date testing and checking function

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,6 @@ function Opportunities(recordsReturnMax, location, distance, startDate, endDate,
   this.recordsReturn = 100;
   this.location = location;
   this.distance = distance;
-  // this.startDate = newStartDate(startDate, endDate);
-  // this.endDate = newEndDate(startDate, endDate);
   this.startDate = startDate;
   this.endDate = endDate;
   this.keyword = keyword;
@@ -15,46 +13,18 @@ function Opportunities(recordsReturnMax, location, distance, startDate, endDate,
   this.items =[];
   this.jsonString = "http://api2.allforgood.org/api/volopps?&key=epicodus&num=" + this.recordsReturn + "&type=all&merge=3&output=json-hoc&vol_loc=" + this.location + "&vol_dist=" + this.distance +  "&vol_startdate=" + this.startDate +  this.endDate + "&output=json-hoc&sort=" + this.sort + "&q=" + this.keyword + " ";
 };
-var convertedStartdate = function(startDate, endDate){
-  var newStartDate = new Date(startDate);
-  var newEndDate = new Date(endDate);
-  var newStartdateNumber = Date.parse(startDate);
-  var newEnddateNumber = Date.parse(endDate);
-  var currentDateNumber = Number(new Date());
-  var newStartYear = newStartDate.getFullYear();
-  var newStartMonth = newStartDate.getMonth();
-  var newStartDay = newStartDate.getDate();
-  var newEndYear = newEndDate.getFullYear();
-  var newEndMonth = newEndDate.getMonth();
-  var newEndDay = newEndDate.getDate();
-  var newCurrentYear = new Date().getFullYear();
-  var newCurrentMonth = new Date().getMonth();
-  var newCurrentDay = new Date().getDate();
-  // if(newStartYear === newEndYear && newStartMonth === newEndMonth && newStartDay === newEndDay){
-  //   return startDate;
-  // } else {
-  //   return startDate;
-  // };
-  // if(newStartYear === newEndYear && newStartMonth === newEndMonth && newEndDate - newStartDate <= 30){
-  //   return startDate;
-  // } else {
-  //   return startDate;
-  // };
-  if(startDate === ""){
-    startDate = "NOW"
+var checkDates = function(startDate, endDate){
+  var newStartDate = new Date();
+  var newEndDate = new Date();
+  if(startDate !== "" && startDate !== "NOW"){
+    newStartDate = new Date(startDate);
   };
-  return startDate;
-};
-var convertedEnddate = function(startDate, endDate){
-  var newStartDate = new Date(startDate);
-  var newEndDate = new Date(endDate);
-  var newStartdateNumber = Date.parse(startDate);
-  var newEnddateNumber = Date.parse(endDate);
-  var currentDateNumber = Number(new Date());
-  if (endDate !== ""){
-    endDate = "&vol_enddate=" + endDate;
+  if(endDate !== ""){
+    newEndDate = new Date(endDate);
   };
-  return endDate;
+  var newStartdateNumber = newStartDate.getFullYear() + newStartDate.getMonth() + newStartDate.getDate();
+  var newEnddateNumber = newEndDate.getFullYear() + newEndDate.getMonth() + newEndDate.getDate();
+  return newEnddateNumber >= newStartdateNumber;
 };
 var jsonOportunities = function(newOpportunities) {
   $.ajaxSetup({
@@ -63,8 +33,8 @@ var jsonOportunities = function(newOpportunities) {
 
   $.getJSON(newOpportunities.jsonString, function(jsonOpportunities) {
     newOpportunities.totalOpportunities = jsonOpportunities.TotalOpportunities;
+    console.log(jsonOpportunities);
     newOpportunities.recordsReturn = jsonOpportunities.num;
-    console.log(newOpportunities.jsonString);
     if (jsonOpportunities.num > 0) {
       for (var i = 0; i < jsonOpportunities.items.length; i++) {
          newOpportunities.items.push(jsonOpportunities.items[i]);
@@ -100,15 +70,12 @@ var newDistanceNumber = function(newDistance){
   };
   return newDistanceNumber;
 };
-
 //interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    var newRecordsReturnMax = 100;
     $("#newOpportunities").empty();
-    //defaults
-    var recordsReturnMax = 100;
-    //end defaults
     var newSerchCriteria = "";
     var newKeyword = $("input#insert-text").val();
     if(newKeyword !== ""){
@@ -122,39 +89,41 @@ $(document).ready(function() {
     var newDistance = newDistanceNumber($("select#distance").val());
     var newStartdate = $("input#start-date").val();
     var newEnddate = $("input#end-date").val();
-    var startDate = convertedStartdate(newStartdate, newEnddate);
-    var endDate = convertedEnddate(newStartdate, newEnddate);
-
-    $("input#insert-text").val('');
-    $("select#categories").val('');
-    $("input#location").val('');
-    $('select#distance').val( $('select#distance').prop('defaultSelected') );
-
-    // var keyword = "hunger homelessness OR Health & Wellness";
-    var newOpportunities = new Opportunities(recordsReturnMax, newLocation, newDistance, startDate, endDate, newSerchCriteria);
-    jsonOportunities(newOpportunities);
-
-    if(newOpportunities.recordsReturn > 0){
-      for (var i = 0; i < newOpportunities.items.length; i++) {
-        newOpportunities.items[i];
-        var newDistance = parseFloat(newOpportunities.items[i].Distance).toFixed(2);
-        $("#newOpportunities").append("<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 border'>" +
-                                      "<h4>" + newOpportunities.items[i].title + "</h4>" +
-                                      "<p>" +  newOpportunities.items[i].description + "...</p>" +
-                                      "<ul>" +
-                                        "<li>"+newOpportunities.items[i].location_name +"</li>" +
-                                        "<li>"+ newDistance +"</li>" +
-                                        "<li><a href='" + newOpportunities.items[i].detailUrl + "'>" + "Click here for the link" + "</a></li>" +
-                                        "<li>"+newOpportunities.items[i].startDate +"</li>" +
-                                        "<li>"+newOpportunities.items[i].endDate +"</li>" +
-                                        "<li>"+newOpportunities.items[i].volunteerHubOrganizationName +"</li>" +
-                                        "<li><a href='" + newOpportunities.items[i].volunteerHubOrganizationUrl + "'>" + "Click here for info about the organization" + "</a></li>" +
-                                      "<ul></div>");
+    if(checkDates(newStartdate, newEnddate)){
+      if(newStartdate === ""){
+        newStartdate = "NOW"
       };
+      if (newEnddate !== ""){
+        newEnddate = "&vol_enddate=" + newEnddate;
+      };
+      var newOpportunities = new Opportunities(newRecordsReturnMax, newLocation, newDistance, newEnddate, newEnddate, newSerchCriteria);
+      jsonOportunities(newOpportunities);
 
-    } else {
-      alert("No results found. Try changing your search criteria.")
+      if(newOpportunities.recordsReturn > 0){
+        for (var i = 0; i < newOpportunities.items.length; i++) {
+          newOpportunities.items[i];
+          var newDistance = parseFloat(newOpportunities.items[i].Distance).toFixed(2);
+          $("#newOpportunities").append("<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 border'>" +
+                                        "<h4>" + newOpportunities.items[i].title + "</h4>" +
+                                        "<p>" +  newOpportunities.items[i].description + "</p>" +
+                                        "<ul>" +
+                                          "<li>"+newOpportunities.items[i].location_name +"</li>" +
+                                          "<li>"+ newDistance +"</li>" +
+                                          "<li><a href='" + newOpportunities.items[i].detailUrl + "'>" + "Click here for the link" + "</a></li>" +
+                                          "<li>"+newOpportunities.items[i].startDate +"</li>" +
+                                          "<li>"+newOpportunities.items[i].endDate +"</li>" +
+                                          "<li>"+newOpportunities.items[i].volunteerHubOrganizationName +"</li>" +
+                                          "<li><a href='" + newOpportunities.items[i].volunteerHubOrganizationUrl + "'>" + "Click here for info about the organization" + "</a></li>" +
+                                        "<ul></div>");
+        };
+
+      }
+      else {
+        alert("No results found. Try changing your search criteria.")
+      };
+    }
+    else{
+      alert("The end date must be greater than the start date.")
     };
-
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,10 @@
 describe('Opportunities', function () {
   it("creates a new object Opportunities with given properties", function() {
-    var testOpportunities = new Opportunities(100, "Portland, OR", 15, "2016-02-05", "2016-02-30", "hunger homelessness OR Health & Wellness");
+    var testOpportunities = new Opportunities(1,"Portland, OR", 15, "2016-02-05", "2016-02-30", "hunger homelessness OR Health & Wellness");
     jsonOportunities(testOpportunities);
-    expect(testOpportunities.recordsReturn).to.equal(100);
-    expect(testOpportunities.items.length).to.equal(100);
+    expect(testOpportunities.recordsReturnMax).to.equal(1);
+    expect(testOpportunities.items[0].location_name).to.equal("Portland OR United States ");
+
   });
 });
 describe('Distance', function () {
@@ -12,23 +13,9 @@ describe('Distance', function () {
     expect(testDistance).to.equal(15);
   });
 });
-// describe('DateValue', function () {
-//   it("returns a new startdate from NewStartdate selection", function() {
-//     var testStartDate = newStartdate("2016-02-05", "2016-02-05");
-//     expect(testStartDate).to.equal("2016-02-05");
-//   });
-//   it("returns a new startdate from NewStartdate selection", function() {
-//     var testStartDate = newStartdate("2016-02-05", "2016-02-25");
-//     expect(testStartDate).to.equal("2016-02-05");
-//   });
-  // it("returns a new startdate from NewStartdate selection", function() {
-  //   var testStartDate = newStartdate("2016-01-05", "2016-03-02");
-  //   expect(testStartDate).to.equal("NOW");
-  // });
-
-
-//   it("returns a new enddate from NewEnddate selection", function() {
-//     var testEndDate = newEnddate("2016-02-05", "2016-02-05");
-//     expect(testEndDate).to.equal("");
-//   });
-// });
+describe('Date check', function () {
+  it("return true if endDate is > startdate", function() {
+    var testDate = checkDates("2016-02-05", "2016-02-05");
+    expect(testDate).to.equal(true);
+  });
+});
